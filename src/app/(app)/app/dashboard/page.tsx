cc67b32b7a92e00eda4917a863a8df3c89c8b72e
Олег bc2024-5 <olegchaplia2006@gmail.com>
2025-07-28 10:36:32 +0300

    feat: add branding and stats components

--- src/app/(app)/app/dashboard/page.tsx
+++ src/app/(app)/app/dashboard/page.tsx
@@ -1,5 +1,16 @@
+import Branding from "@/components/branding/Branding";
+import H1 from "@/components/h1/H1";
+import Stats from "@/components/stats/Stats";
+
 const DashboardPage = () => {
-  return <>DASHBOARD</>;
+  return (
+    <main>
+      <div className="flex justify-between items-center text-white py-8">
+        <Branding />
+        <Stats />
+      </div>
+    </main>
+  );
 };
 
 export default DashboardPage;
---
+++ src/components/stats/Stats.tsx
@@ -0,0 +1,10 @@
+const Stats = () => {
+  return (
+    <section>
+      <p className="text-2xl font-bold leading-6">2</p>
+      <p className="opacity-80">current guest</p>
+    </section>
+  );
+};
+
+export default Stats;
